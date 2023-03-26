2311d209e93d4b4bb1284301e2d0c24d804019a4
Alok Kumar Verma <alok.mcaet@gmail.com>
2023-03-26 13:37:31 +0530

    added

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,49 +1,51 @@
-const {UserModel}=require("../model/user.model")
+const { UserModel } = require("../model/user.model");
 
 const express = require("express");
 const UserRouter = express.Router();
 var jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
-const userRouter=express.Router()
+const userRouter = express.Router();
 
-userRouter.post("/register",(req,res)=>{
+userRouter.post("/register", (req, res) => {
+  const { name, email, pass } = req.body;
 
-    const {name,email,pass}=req.body
+  try {
+    bcrypt.hash(pass, 5, async function (err, hash) {
+      // Store hash in your password DB.
+      await UserModel.create({ name, email, pass: hash });
+      res.status(200).send({ msg: "user created" });
+    });
+  } catch (error) {
+    res.status(400).send({ msg: error });
+  }
+});
 
-    try {
-        bcrypt.hash(pass, 5,async function(err, hash) {
-            // Store hash in your password DB.
-            await UserModel.create({name,email,pass:hash})
-            res.status(200).send({"msg":"user created"})
-        });
-    } catch (error) {
-        res.status(400).send({"msg":error})
+userRouter.post("/login", async (req, res) => {
+  const { email, pass } = req.body;
+  try {
+    let user = await UserModel.findOne({ email });
+    if (user) {
+      let hash = await bcrypt.compare(pass, user.pass);
+      if (!hash) {
+        return res.status(401).send("incorrect password");
+      } else {
+        const token = jwt.sign(
+          {
+            _id: user._id,
+            email: user.email,
+          },
+          "secret"
+        );
+        return res
+          .status(200)
+          .send({ token, user, message: "Login Successfully" });
+      }
     }
-})
+  } catch (error) {
+    return res.status(404).send(error.message);
+  }
+});
 
-userRouter.post("/login",async(req,res)=>{
-    const {email,pass}=req.body
-
-    try {
-        const user=await UserModel.findOne({email})
-        if(user){
-            bcrypt.compare(pass, user.pass, function(err, result) {
-                // result == true
-                if(result){
-                    const token=jwt.sign({userID:user._id},"secret")
-                   return res.status(200).send({token})
-                }else{
-                    return res.status(400).send({"msg":"Unauthorised"})
-                }
-            });
-        }else{
-            return res.status(400).send({"msg":"user does not exist"})
-        }
-    } catch (error) {
-        return res.status(401).send({"msg":error.message})
-    }
-})
-
-module.exports={
-    userRouter
-}
+module.exports = {
+  userRouter,
+};
