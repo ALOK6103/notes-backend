0ff25a17a797694293a90a31da7c5cf22fe2aac3
Alok Kumar Verma <alok.mcaet@gmail.com>
2023-03-26 13:11:27 +0530

    added backend part for the notes app

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-const mongoose=require("mongoose")
-const express=require("express")
-const {connection}=require("./db")
-const {userRouter}=require("./routes/user.route")
-const {noteRouter}=require("./routes/note.router")
-const {isAuth}=require("./middleware/auth.middleware")
-require("dotenv").config()
-const cors=require("cors")
-const app=express()
-app.use(express.json())
-app.use(cors())
+require("dotenv").config();
+const mongoose = require("mongoose");
+const express = require("express");
+const { connection } = require("./db");
+const { userRouter } = require("./routes/user.route");
+const { noteRouter } = require("./routes/note.router");
+const { isAuth } = require("./middleware/auth.middleware");
 
-app.get("/",(req,res)=>{
-    res.send("hello")
-})
+const cors = require("cors");
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: "*" }));
 
-app.use("/users",userRouter)
-app.use(isAuth)
-app.use("/notes",noteRouter)
-app.listen(process.env.port,async()=>{
-    try {
-        await connection
-        console.log("connected")
-    } catch (error) {
-        console.log(error)
-    }
-})
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+app.use("/users", userRouter);
+app.use(isAuth);
+app.use("/notes", noteRouter);
+app.listen(process.env.port, async () => {
+  try {
+    await connection;
+    console.log("connected");
+  } catch (error) {
+    console.log(error);
+  }
+});
